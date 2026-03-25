ce2503bfa3b79d94f9f47915aa7c10cb73b60ef2
Art1kDev <Aleksashademin@yandex.ru>
2026-03-25 20:44:17 +0300

    Create BestScore.java

---
+++ com/art1kdev/ZombieTown/online/BestScore.java
@@ -0,0 +1,10 @@
+package com.art1kdev.Zombietown.online;
+
+import java.io.*;
+import java.util.*;
+
+public class BestScore {
+    private static final String SAVE_FILE = "best_scores.dat";
+    private Map<String, Integer> bestScores = new HashMap<>();
+
+}
